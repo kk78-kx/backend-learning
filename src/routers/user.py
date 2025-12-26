94bc5a29fb69d0b378ee8042d9bc81de56e71eb5
kittiphat78 <chinnaphat.rot@gmail.com>
2025-12-26 20:30:32 +0700

    Day 5: add create user logic and fix imports

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
-from src.models.user import User
-from src.services.user_service import get_all_users
+from src.models.user import User, UserCreate
+from src.services.user_service import get_all_users, create_user
+
+
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/", response_model=list[User])
 def get_users():
     return get_all_users()
+
+@router.post("/", response_model=User)
+def create(user: UserCreate):
+    return create_user(user)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,10 +1,26 @@
-from src.models.user import User
+from src.models.user import User, UserCreate
 
 __fake_db = [
     User(id=1, name="Alice", age=30),
     User(id=2, name="Bob", age=25),
 ]
 
+_next_id = 3
+
 def get_all_users() -> list[User]:
     """Return the list of all users."""
     return __fake_db
+
+def create_user(user: UserCreate) -> User:
+    global __next_id
+
+    new_user = User(
+        id=__next_id, 
+        name=user.name, 
+        age=user.age
+        )
+    
+    __fake_db.append(new_user)
+    __next_id += 1
+    
+    return new_user
